af8dc7907ffddcd6909224b2f00a70834f3a4c15
innersanctumentertainment <75913396+innersanctumentertainment@users.noreply.github.com>
2026-09-10 21:59:22 -0400

    Add critical PAJPYS application files for production deploy

--- routes/auth.php
+++ routes/auth.php
@@ -1,1 +1,23 @@
-LOAD_FROM_FILE
+<?php
+
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store'])->name('register.store');
+
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    Route::post('login', [LoginController::class, 'store'])->name('login.store');
+
+    Route::get('email/verify-guest/{id}/{hash}', [EmailVerificationController::class, 'verifySigned'])
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify.guest');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+    Route::get('email/verify', [EmailVerificationController::class, 'notice'])->name('verification.notice');
+});
